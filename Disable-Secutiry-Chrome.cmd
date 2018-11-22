bb588ff79db62ffe5a99d77e0952afa1ddd8bcce
xfalcon <x-falcon@users.noreply.github.com>
2018-11-22 13:31:09 +0800

    Update Disable-Secutiry-Chrome.cmd

--- Disable-Secutiry-Chrome.cmd
+++ Disable-Secutiry-Chrome.cmd
@@ -4,5 +4,5 @@ for /f "tokens=2,*" %%A in ('reg query "HKEY_LOCAL_MACHINE\SOFTWARE\Clients\Star
 if not defined Command for /f "tokens=2,*" %%A in ('reg query "HKEY_LOCAL_MACHINE\SOFTWARE\Wow6432Node\Clients\StartMenuInternet\Google Chrome\shell\open\command" /ve 2^>nul') do set "Command=%%~B"
 if not defined Command echo Google Chrome was not found.
 set userdata="%tmp%/tempChrome"
-if defined Command  start ""  "%Command%"   --no-first-run  --disable-web-security --user-data-dir="%userdata%"
-exit
+set html="%~dp0%\index.html"
+if defined Command  start ""  "%Command%"   --no-first-run  --disable-web-security --user-data-dir="%userdata%" file:///%html%
